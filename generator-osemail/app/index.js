d2abee00fd8b9f4afce236bbf79dc643891892c6
Vincent McAulay <bwownbwead@hotmail.com>
2016-01-26 08:11:21 +0000

    adding gitignore, updating index template

--- generator-osemail/app/index.js
+++ generator-osemail/app/index.js
@@ -2,10 +2,52 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-	method1: function () {
-    	console.log('Hello');
-  	},
-  	method2: function () {
-    	console.log('World!');
-  	}
+
+	// constructor: function() {
+
+	// },
+	
+	initializing: function () {
+
+	},
+
+	prompting: function () {
+		
+	},
+
+	configuring: function () {
+		
+	},
+
+	default: function () {
+		
+	},
+
+	writing: {
+		appStaticFiles: function () {
+			this.directory('images', 'src/images');
+		},
+
+		html: function () {
+			this.fs.copyTpl(
+				this.templatePath('_index.html'),
+				this.destinationPath('src/index.html'),
+				{
+					yzname: 'Test Youth Zone'
+				}
+			)
+		}
+	},
+
+	conflicts: function () {
+		
+	},
+
+	install: function () {
+
+	},
+
+	end: function () {
+
+	}
 });
