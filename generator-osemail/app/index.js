0dd40c04e4036aa3d912c159fd38c604a5c9c222
Vincent McAulay <bwownbwead@hotmail.com>
2016-01-31 23:05:13 +0000

    added a load more options

--- generator-osemail/app/index.js
+++ generator-osemail/app/index.js
@@ -16,7 +16,6 @@ module.exports = generators.Base.extend({
 		
 		if ( this.yzname ) {
 			this.yzname = _.startCase(this.yzname);
-			this.log(this.yzname);
 		}
 		
 		// this.option('testbool', {
@@ -37,6 +36,12 @@ module.exports = generators.Base.extend({
 	},
 
 	prompting: function () {
+
+		var defaultAnswers = {
+			twitterUrl: 'Youth zone doesn\'t use Twitter',
+			linkedInUrl: 'Youth zone doesn\'t use LinkedIn'
+		}
+
 		this.log(yosay('Welcome to the ' + chalk.blue('Onside Youth Zone Email') + ' generator'));
 
 		var done = this.async();
@@ -47,20 +52,66 @@ module.exports = generators.Base.extend({
 			name: 'charityNumber',
 			message: 'What is the registered charity number?',
 			default: 'XXXXXXX',
-			store: true
+		},
+		{
+			type: 'input',
+			name: 'yzSiteUrl',
+			message: 'What is the URL of the youth zone\'s website?',
+			default: 'http://www.onsideyouthzones.org/',
+		},
+		{
+			type: 'input',
+			name: 'footerBgColour',
+			message: 'What is the colour of the footer\'s background?',
+			default: '#e4e4e4'
 		},
 		{
 			type: 'input',
 			name: 'colourBodyCopy',
-			message: 'What is the colour of the body copy (hex value)?',
+			message: 'What is the colour of the body copy?',
 			default: '#444444'
-
 		},
 		{
 			type: 'input',
-			name: 'colourHeadingCopy',
-			message: 'What is the colour of the main heading (hex value)?',
-			default: '#ff0000'
+			name: 'primaryBrandColourName',
+			message: 'What is the primary brand colour name?',
+			default: 'Green'
+		},
+		{
+			type: 'input',
+			name: 'primaryBrandColour',
+			message: 'What is the primary brand colour?',
+			default: '#cedf00'
+		},
+		{
+			type: 'input',
+			name: 'secondaryBrandColourName',
+			message: 'What is the primary brand colour name?',
+			default: 'Purple'
+		},
+		{
+			type: 'input',
+			name: 'secondaryBrandColour',
+			message: 'What is the primary brand colour?',
+			default: '#592c82'
+		},
+		{
+			type: 'input',
+			name: 'darkBgColour',
+			message: 'What is the dark background colour (image-left, text-right section)?',
+			default: '#592c82'
+		},
+		{
+			type: 'input',
+			name: 'twitterUrl',
+			message: 'Does the youth zone have a twitter account? If so what is the profile URL?',
+			default: defaultAnswers.twitterUrl
+		},
+		{
+			type: 'input',
+			name: 'linkedInUrl',
+			message: 'Does the youth zone have a LinkedIn account? If so what is the profile URL?',
+			default: defaultAnswers.linkedInUrl
 		},
 		{
 			type: 'checkbox',
@@ -94,17 +145,35 @@ module.exports = generators.Base.extend({
 				}
 			]
 		}], function(answers) {
-			this.charityNumber = answers.charityNumber;
-			// this.colourBodyCopy = answers.colourBodyCopy;
-			this.config.set('colourBodyCopy', answers.colourBodyCopy);
-			this.config.save();
 
+			// basic information
+			this.yzSiteUrl = answers.yzSiteUrl;
+			this.charityNumber = answers.charityNumber;
+
+			// urls
+			this.twitterUrl = ( defaultAnswers.twitterUrl === answers.twitterUrl ) ? false : answers.twitterUrl;
+			this.linkedInUrl = ( defaultAnswers.linkedInUrl === answers.linkedInUrl ) ? false : answers.linkedInUrl;
+			
+			// colours
+			this.config.set('colourBodyCopy', answers.colourBodyCopy);
+			this.config.set('darkBgColour', answers.darkBgColour);
+			this.config.set('buttonBgColour', answers.buttonBgColour);
+			this.footerBgColour = answers.footerBgColour;
+			this.primaryBrandColour = answers.primaryBrandColour;
+			this.primaryBrandColourName = answers.primaryBrandColourName;
+			this.secondaryBrandColour = answers.secondaryBrandColour;
+			this.secondaryBrandColourName = answers.secondaryBrandColourName;
+
+			// layout
 			this.layoutSingleWideColumn = _.includes(answers.layoutSections, 'layoutSingleWideColumn');
 			this.layoutTwoColumns = _.includes(answers.layoutSections, 'layoutTwoColumns');
 			this.layoutThreeColumns = _.includes(answers.layoutSections, 'layoutThreeColumns');
 			this.layoutImageLeftTextRight = _.includes(answers.layoutSections, 'layoutImageLeftTextRight');
 			this.layoutImageRightTextLeft = _.includes(answers.layoutSections, 'layoutImageRightTextLeft');
+
+			this.config.save();
 			done();
+
 		}.bind(this));
 
 	},
@@ -127,9 +196,26 @@ module.exports = generators.Base.extend({
 				this.templatePath('_index.html'),
 				this.destinationPath('src/index.html'),
 				{
+					// basic information
 					yzname: this.yzname,
 					charityNumber: this.charityNumber,
+
+					// urls
+					yzSiteUrl: this.yzSiteUrl,
+					twitterUrl: this.twitterUrl,
+					linkedInUrl: this.linkedInUrl,
+
+					// colours
 					colourBodyCopy: this.config.get('colourBodyCopy'),
+					darkBgColour: this.config.get('darkBgColour'),
+					buttonBgColour: this.config.get('buttonBgColour'),
+					primaryBrandColour: this.primaryBrandColour,
+					primaryBrandColourName: this.primaryBrandColourName,
+					secondaryBrandColour: this.secondaryBrandColour,
+					secondaryBrandColourName: this.secondaryBrandColourName,
+					footerBgColour: this.footerBgColour,
+
+					// layout
 					layoutSingleWideColumn: this.layoutSingleWideColumn,
 					layoutTwoColumns: this.layoutTwoColumns,
 					layoutThreeColumns: this.layoutThreeColumns,
