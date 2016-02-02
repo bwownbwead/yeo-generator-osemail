6e11e2eb64925b8997bd7ffa8b850f7be40015e9
Vincent McAulay <v.mcaulay@amaze.com>
2016-02-02 09:08:18 +0000

    added some testing with prompts

--- generator-osemail/test/test-app.js
+++ generator-osemail/test/test-app.js
@@ -9,20 +9,40 @@ describe('oseamil:app', function () {
 	describe('default', function () {
 		before(function (done) {
 			helpers.run(path.join(__dirname, '../app'))
-				.withArguments(['testEmail'])
+				.withArguments(['testOnsideYouthZone'])
 				// .withOptions({ skipInstall: true })
 				.on('end', done);
 		});
 
 		it('creates files', function () {
 			assert.file([
-				'package.json',
 				'src/index.html'
-			])
+			]);
 		});
 
-		it('adds email app', function () {
+		it('adds the youth zone name', function () {
+			assert.fileContent('src/index.html', /Test Onside Youth Zone/);
+		});
+	});
 
+	describe('oseamil prompt', function () {
+		before(function (done) {
+			helpers.run(path.join(__dirname, '../app'))
+				.withPrompts({
+					'charityNumber': '12345ABCD',
+					'primaryBrandColourName': 'Orange',
+					'secondaryBrandColourName': 'Blue'
+				})
+				.on('end', done);
+		});
+
+		it('adds coloured headings', function () {
+			assert.fileContent('src/index.html', /\.HeadlineOne[A-Z]*[a-z]*/);
+			assert.fileContent('src/index.html', /\.HeadlineTwo[A-Z]*[a-z]*/);
+		});
+
+		it('adds the registered charity number', function () {
+			assert.fileContent('src/index.html', /Registered Charity No:\s*[0-9|a-z|A-Z]+/);
 		});
 	});
 });
